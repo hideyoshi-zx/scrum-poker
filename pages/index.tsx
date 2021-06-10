94b01c856a4c1d151b392776f0dbbe017f0d9455
Yusuke Kinoshita <yusuke.kinoshita@knowledgelabo.com>
2021-06-10 23:35:33 +0900

    コメントを削除

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,74 +3,11 @@ import { useRouter } from 'next/router'
 import firebase from '../lib/firebaseClient';
 import { nanoid } from 'nanoid'
 
-// カスタムフックにしておく
-// const useDatabase = () => {
-//   // 同じパスでは毎回同じ結果が得られるのでmemo化しておく
-//   return useMemo(() => firebase.database().ref('/sample'), []);
-// };
-
-// hooksを使いたいのでカスタムhooksにしておく
-// const useFetchData = (ref: firebase.database.Reference, user: object | null) => {
-//   const [data, setData] = useState<{[key: string]: string}>();
-//   useEffect(() => {
-//         // イベントリスナーを追加するにはonを使う
-//     ref.on('value', snapshot => {
-//             // パスに対する全データを含むsnapshotが渡される
-//             // ない場合はnullが変えるので存在をチェックしておく
-//       if (snapshot?.val()) {
-//         setData(snapshot.val());
-//       }
-//     });
-//     return () => {
-//       ref.off();
-//     };
-//     // refの変更に応じて再取得する
-//   }, [ref, user]);
-//     // データを返却する
-//   return data;
-// }
-
-// const useAllEffect = () => {
-//   const [user, setUser] = useState<object | null>(null);
-//   useEffect(() => {
-//     firebase.auth().signInAnonymously().catch( (error) => {
-//       console.log(`[error] Can not signin anonymouse (${error.code}:${error.message})`);
-//     });
-//
-//         // 現在ログインしているユーザを取得
-//     firebase.auth().onAuthStateChanged(user => {
-//       // ユーザ情報が取れればログイン状態
-//       setUser(user);
-//     });
-//   }, [])
-//
-//   // refを取得して
-//   const ref = useDatabase();
-//   // ref渡してデータを取得する
-//   const data = useFetchData(ref, user);
-//
-//   return [user, data];
-// }
-
-// const login = () => {
-//   const [user, setUser] = useState<object | null>()
-//   useEffect(() => {
-//     firebase.auth().signInAnonymously().catch((error) => {
-//       console.log(`[error] Can not signin anonymouse (${error.code}:${error.message})`);
-//     })
-//
-//     firebase.auth().onAuthStateChanged(user => {
-//       setUser(user)
-//     });
-//   }, [])
-//   return user;
-// }
-
 type Props = {
   userId: string;
 }
 
-export default function Home({ userId }: Props) {
+export default function Home(_props: Props) {
   const router = useRouter()
 
   const createRoom = () => {
@@ -80,21 +17,4 @@ export default function Home({ userId }: Props) {
   }
 
   return <button type="button" onClick={createRoom}>スクラムポーカーを始める</button>;
-
-  // const { data: { user } } = useSWR('');
-
-  // const { data, error } = useSWR('/');
-  // const [user, data] = useAllEffect();
-  // const dataList = useMemo(() => Object.entries(data || {}).map(([key, value]) => ({ key, value })), [data]);
-
-  // if (!user) {
-  //   return <div>not logged in</div>
-  // }
-
-  // return <dl>{dataList.map(({ key, value }) =>
-  //   <React.Fragment key={`${key}${value}`}>
-  //     <dt>key: {key}</dt>
-  //     <dt>value: {value}</dt>
-  //   </React.Fragment>
-  // )}</dl>
 }

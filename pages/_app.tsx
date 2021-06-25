987094ca1590b55cf30284716e5ae949eed76be0
Yusuke Kinoshita <yusuke.kinoshita@knowledgelabo.com>
2021-06-25 19:52:07 +0900

    匿名登録時に名前入力できるように

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,42 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { useCurrentUser } from '../usecases/auth'
-import React from 'react'
+import { useUserId, useCurrentUser, createUser } from '../usecases/auth'
+import React, { useState } from 'react'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  const currentUser = useCurrentUser()
+export default function MyApp(props : AppProps) {
+  const userId = useUserId()
+  if (!userId) return <div>Loading...</div>
 
-  if (!currentUser) return <div>Loading...</div>
+  return <LoggedIn {...props} userId={userId} />
+}
 
-  const props = { currentUser, ...pageProps }
+function LoggedIn ({ Component, pageProps, userId } : AppProps & { userId : string }) {
+  const currentUserResult = useCurrentUser(userId)
+  if (currentUserResult.error === 'NotFound') return <UserForm userId={userId} />
+  if (!currentUserResult.data) return <div>Loading</div>
 
   return (
     <div>
-      <Component {...props} />
+      <Component {...pageProps} currentUser={currentUserResult.data} />
     </div>
   )
 }
-export default MyApp
+
+function UserForm ({ userId } : { userId : string }) {
+  const [name, setName] = useState('')
+
+  const handleChange = (event : React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value)
+  }
+
+  const handleSubmit = (event : React.FormEvent<HTMLFormElement>) => {
+    createUser(userId, name)
+    event.preventDefault()
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={name} onChange={handleChange} placeholder="Input your name" />
+    </form>
+  )
+}
--- usecases/auth.ts
+++ usecases/auth.ts
@@ -1,33 +1,7 @@
 import firebase from '../utils/firebaseClient';
 import { useEffect, useState, useMemo } from 'react';
 import { User } from '../types';
-
-export function useCurrentUser () {
-  const [user, setUser] = useState<User>()
-  const userId = useUserId()
-  const ref = useMemo(() => userId && firebase.database().ref('users/' + userId), [userId])
-
-  useEffect(() => {
-    if (!ref) return
-
-    const data = {
-      id: userId,
-      name: 'Anonymouse',
-    }
-
-    ref.on('value', snapshot => {
-      if (snapshot?.val()) {
-        setUser(snapshot.val())
-      } else {
-        ref.set(data)
-      }
-    });
-
-    return () => { ref.off() }
-  }, [ref])
-
-  return user
-}
+import { Result, loading, succeeded, failed } from '../utils/result'
 
 export function useUserId () {
   const [userId, setUserId] = useState<string | undefined>()
@@ -41,4 +15,34 @@ export function useUserId () {
     });
   }, [])
   return userId;
+}
+
+export type CurrentUserResult = Result<User, 'NotFound'>
+export function useCurrentUser (userId : string) {
+  const [user, setUser] = useState<CurrentUserResult>(loading)
+  const ref = useUserRef(userId)
+
+  useEffect(() => {
+    setUser(loading)
+
+    ref.on('value', snapshot => {
+      if (snapshot?.val()) {
+        setUser(succeeded(snapshot.val()))
+      } else {
+        setUser(failed('NotFound'))
+      }
+    });
+
+    return () => { ref.off() }
+  }, [ref])
+
+  return user
+}
+
+export function createUser (id : string, name : string) {
+  firebase.database().ref(`users/${id}`).set({ id, name })
+}
+
+function useUserRef (userId : string) {
+  return useMemo(() => firebase.database().ref('users/' + userId), [userId])
 }

28b13107c04bf7e17e278039e271090f30177c83
Yusuke Kinoshita <yusuke.kinoshita@knowledgelabo.com>
2021-07-01 17:42:48 +0900

    Tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,5 +11,15 @@ export default function Home(_props: PageProps) {
     router.push(roomId)
   }
 
-  return <button type="button" onClick={handleClick}>スクラムポーカーを始める</button>;
+  // return <button type="button" onClick={handleClick}>スクラムポーカーを始める</button>;
+
+  return (
+    <button
+      onClick={handleClick}
+      type="button"
+      className="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+    >
+      スクラムポーカーを始める
+    </button>
+  )
 }

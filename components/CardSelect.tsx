00e0d98403883f60bb1c709045564aeb445f0b9f
Yusuke Kinoshita <yusuke.kinoshita@knowledgelabo.com>
2021-07-02 16:06:04 +0900

    Tweak

--- components/CardSelect.tsx
+++ components/CardSelect.tsx
@@ -8,15 +8,18 @@ type Props = {
 
 export default function CardSelect ({ selected, onChange }: Props) {
   return (
-    <div className="w-min mx-auto flex justify-center py-4 px-6 space-x-4 bg-white rounded">
-      { CARDS.map(card => <CardButton card={card} selected={selected} onClick={onChange} />) }
-    </div>
+    <>
+      <p className="text-center text-gray-500">Choose your card</p>
+      <div className="mt-4 w-min mx-auto flex justify-center py-4 px-6 space-x-4 bg-white rounded">
+        { CARDS.map(card => <CardButton card={card} selected={selected} onClick={onChange} />) }
+      </div>
+    </>
   )
 }
 
 function CardButton ({ card, selected, onClick }: { card: Card, selected: Card | '', onClick: (card: Card) => any }) {
   const isSelected = card === selected
-  const normalClassName = 'h-16 w-12 text-xl font-bold rounded shadow-inner text-blue-500 bg-blue-50 hover:bg-blue-100 transition-all'
+  const normalClassName = 'h-16 w-12 text-xl font-bold rounded border-2 border-blue-100 text-blue-400 bg-blue-50 hover:bg-blue-100 transition-all'
   const activeClassName = 'h-16 w-12 text-xl font-bold rounded shadow-md bg-blue-500 text-white transform -translate-y-3 transition-all cursor-default'
   const className = isSelected ? activeClassName : normalClassName
 
